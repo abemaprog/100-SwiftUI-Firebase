41eac62fdce94bcbe737fa464debae67f0bc8d18
abemaprog <appdev.abe@gmail.com>
2024-11-10 16:34:42 +0900

    [完成] 新規登録画面 ログイン画面

---
+++ LoveMemory/Views/Coponents/CustomHeader.swift
@@ -0,0 +1,18 @@
+//
+//  CustomHeader.swift
+//  LoveMemory
+//
+//  Created by Manato Abe on 2024/11/10.
+//
+
+import SwiftUI
+
+struct CustomHeader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomHeader()
+}
